760a656b982350be552b4153f58ebfc7aa4a1060
Apaza Oscar <Apaza Oscar@DESKTOP-2QAPK9M>
2020-06-16 03:11:22 -0300

    Asunto: Inicializacion del Proyecto Final -- Responsables: Apaza, Oscar -Emanuel, Alavar - Miranda, Sebastian - Sajama, Fabian ---  Task000 ---

---
+++ src/main/java/ar/edu/unju/fi/ProyectoFinalGrupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalGrupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalGrupo10Application.class, args);
+	}
+
+}
