d3964dde1d25da95306473d56aa3b7cf5e6db23f
Apaza Oscar <Apaza Oscar@DESKTOP-2QAPK9M>
2020-06-29 01:25:51 -0300

    Validacion de datos y agregado del boton eliminar (funcional) y editar (falta) - Autor: Apaza, Oscar

--- src/main/java/ar/edu/unju/fi/service/IUsuarioService.java
+++ src/main/java/ar/edu/unju/fi/service/IUsuarioService.java
@@ -8,6 +8,9 @@ public interface IUsuarioService {
 	public void guardar(Usuario usuario);
 	public List<Usuario> obtenerUsuarios();
 	public Usuario mostrar();
-	public void eliminar();
-	public Usuario modificar();
+	public void eliminar(Long id);
+	public Usuario modificar(Usuario usuario) throws Exception;
+	public Usuario encontrarUsuario(Long id) throws Exception;
 }
+
+
